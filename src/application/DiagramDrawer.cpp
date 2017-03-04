3aec6ac2820548236c2c4590248313265cbf3d1d
benjamin.russell <benrussell.dev@gmail.com>
2017-03-04 13:57:02 -0500

    Updated brain visualization with a color for mating season input

--- src/application/DiagramDrawer.cpp
+++ src/application/DiagramDrawer.cpp
@@ -98,11 +98,13 @@ void DiagramDrawer::DrawBrainMatrix(Graphics& g, Agent* agent, const Rect2f& bou
 			weightColor.Set(1, 1, 0);
 		else if (i == 1)
 			weightColor.Set(1, 0, 1);
-		else if ((int) i < 2 + (config.genes.maxSightResolution * 2))
+		else if (i == 2)
+			weightColor.Set(0.62f, 0.1f, 0.2f);
+		else if ((int) i < 3 + (config.genes.maxSightResolution * 2))
 			weightColor.Set(1, 0, 0);
-		else if ((int) i < 2 + (config.genes.maxSightResolution * 4))
+		else if ((int) i < 3 + (config.genes.maxSightResolution * 4))
 			weightColor.Set(0, 1, 0);
-		else if ((int) i < 2 + (config.genes.maxSightResolution * 6))
+		else if ((int) i < 3 + (config.genes.maxSightResolution * 6))
 			weightColor.Set(0, 0, 1);
 
 		// Fill box color.
